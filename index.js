ae41a0a7c0bec6bf1982b805a942512dc3b575ad
NazPro <najmulbinnurul3@gmail.com>
2022-12-04 17:10:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const app=express()
+const port= process.env.PORT || 5000 ;
+
+
+
+app.listen(port, (req, res)=>{
+    console.log('listening to port ',port);
+})
